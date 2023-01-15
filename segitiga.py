d0428e970dbd22a3bd150ed320e7d13dd30f29c5
ElKarpeto <115634049+ElKarpeto@users.noreply.github.com>
2023-01-15 21:45:38 +0700

    Add files via upload

---
+++ segitiga.py
@@ -0,0 +1,16 @@
+# untuk input nilai dari segitiga
+a = input("input angka yang pengen dijadiin segitiga : ")
+a = int(a)
+
+print("\n")
+# untuk kondisi
+if a <= 10:
+    for i in range(1, a+1):
+        for j in range(i, i*i+1, i):
+            print(j, end=" ")
+        print()
+else:
+    print("kegedean nilai yang kamu input")
+
+print("\n")
+input("tekan enter untuk keluar")
