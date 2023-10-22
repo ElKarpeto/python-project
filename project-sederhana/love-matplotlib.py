f59cd316757f636082c40f6a9674f825281d9289
ElKarpeto <115634049+ElKarpeto@users.noreply.github.com>
2023-10-22 12:49:54 +0700

    membuat code menjadi lebih bagus

--- project-sederhana/love-matplotlib.py
+++ project-sederhana/love-matplotlib.py
@@ -1,27 +1,22 @@
 import matplotlib.pyplot as plt
 import numpy as np
-#untuk menjalankan program ini, kamu harus meng-install library numpy dan matplotlib
 
-# untuk membuat tulisan pada grafik
+# tulisan pada grafik
 tulisan = input('isi tulisan pada grafik : ')
 
 plt.title("Grafik Love")
 
-# nama untuk setiap sumbu
 plt.xlabel("Sumbu x")
 plt.ylabel("Sumbu y")
 
-# untuk data x
 x = np.arange(-2.236, 2.2361, 0.0001)
 
-# untuk fungsi y
 a = np.sin(10 * np.pi * x)
 y = np.power((np.power(x, 2)), 1/3) + 0.9 * a * \
     ((5 - (np.power(x, 2))) ** 0.5)
 
 plt.text(0, -3, tulisan, fontsize=10, ha='center')
 
-# limit grafik
 plt.xlim(-8, 8)
 plt.ylim(-8, 8)
 
--- project-sederhana/segitiga-perkalian-angka.py
+++ project-sederhana/segitiga-perkalian-angka.py
@@ -1,17 +1,12 @@
-# untuk input nilai dari segitiga
 a = input("input angka yang pengen dijadiin segitiga : ")
 a = int(a)
 
 print("\n")
 
-# untuk kondisi
-if a <= 10:
-    for i in range(1, a+1):
-        for j in range(i, i*i+1, i):
-            print(j, end=" ")
-        print()
-else:
-    print("kegedean nilai yang kamu input")
+for i in range(1, a+1):
+    for j in range(i, i*i+1, i):
+        print(j, end=" ")
+    print()
 
 print("\n")
 
