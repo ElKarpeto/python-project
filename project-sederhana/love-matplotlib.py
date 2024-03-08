0a0703e0d5b871b0dde7490373c49df2c77005ad
ALLLL <115634049+ElKarpeto@users.noreply.github.com>
2024-03-08 09:28:35 +0700

    Update love-matplotlib.py
    mengubah ukuran

--- project-sederhana/love-matplotlib.py
+++ project-sederhana/love-matplotlib.py
@@ -1,25 +1,22 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# tulisan pada grafik
-tulisan = input('isi tulisan pada grafik : ')
-
 plt.title("Grafik Love")
 
-plt.xlabel("Sumbu x")
-plt.ylabel("Sumbu y")
+plt.xlabel("x")
+plt.ylabel("y(x)")
 
-x = np.arange(-2.236, 2.2361, 0.0001)
+x = np.arange(-4.4721, 4.4721, 0.0001)
 
 a = np.sin(10 * np.pi * x)
-y = np.power((np.power(x, 2)), 1/3) + 0.9 * a * \
-    ((5 - (np.power(x, 2))) ** 0.5)
+y = np.power((np.power(x, 2)), 1/3) + 1 * a * ((20 - (np.power(x, 2))) ** 0.5)
 
-plt.text(0, -3, tulisan, fontsize=10, ha='center')
+plt.text(0, -5, tulisan, fontsize=10, ha='center')
 
 plt.xlim(-8, 8)
 plt.ylim(-8, 8)
 
 plt.plot(x, y, color="red")
+plt.grid(True)
 
 plt.show()
